d704de7dcb4c8469c1d5d6a7b6e683097d084d04
Gamin8ing <anim8tor.ing@gmail.com>
2024-11-04 19:20:24 +0530

    add forms

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+// const axios = require("axios");
+
 const Landing = () => {
+	// const data = axios.get("/api");
 	return (
 		<div>
 			<h1>Welcome to StashIt</h1>
+
 			<p>Your one-stop solution for managing your stash.</p>
 			<Link to={"/signup"}>Signup </Link>
 			<Link to={"/login"}>Login </Link>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,46 @@
 import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const Login = () => {
+	const formik = useFormik({
+		initialValues: { email: "", name: "" },
+		validationSchema: Yup.object({
+			name: Yup.string()
+				.max(20, "Name should be less than 20charss")
+				.required("Required"),
+			email: Yup.string().email("Not a valid emaill").required("Requ"),
+		}),
+		onSubmit: (val) => {
+			alert("button clicked " + JSON.stringify(val, null, 2));
+		},
+	});
 	return (
 		<>
 			<h1>Login</h1>
+			<form onSubmit={formik.handleSubmit}>
+				<label htmlFor="email">email</label>
+				<input
+					name="email"
+					id="email"
+					type="email"
+					{...formik.getFieldProps("email")}
+				/>
+				{formik.touched.email && formik.errors.email ? (
+					<div>{formik.errors.email}</div>
+				) : null}
+				<label htmlFor="name">name</label>
+				<input
+					name="name"
+					id="name"
+					type="name"
+					{...formik.getFieldProps("name")}
+				/>
+				{formik.touched.name && formik.errors.name ? (
+					<div>{formik.errors.name}</div>
+				) : null}
+				<button type="submit">submit</button>
+			</form>
 		</>
 	);
 };
